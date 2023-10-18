407443004797ac3e351d786575709ea102e5585d
vancetang <vance.tw@gmail.com>
2023-10-18 17:17:23 +0800

    feat: 增加junit tester

--- src/test/java/com/vance/demo/controller/TestControllerTest.java
+++ src/test/java/com/vance/demo/controller/TestControllerTest.java
@@ -1,10 +1,12 @@
 package com.vance.demo.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestControllerTest {
     @Test
     void testTest() {
-
+        int a = 1;
+        Assertions.assertEquals(1, a);
     }
 }
