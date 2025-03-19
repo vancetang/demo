0b10c416d645d21219c96c0b35da5dc0a1e73f6e
Vance <vance.tw@gmail.com>
2025-03-19 11:41:24 +0800

    test: 增加日誌以顯示異常的根本原因類型

--- src/test/java/com/vance/demo/controller/TestControllerTest.java
+++ src/test/java/com/vance/demo/controller/TestControllerTest.java
@@ -49,6 +49,7 @@ public class TestControllerTest {
         });
         // 檢查異常的根本原因是否符合預期
         Throwable cause = exception.getCause();
+        log.info("{} => {}", url, cause.getClass().getSimpleName());
         assertTrue(expectedCause.isInstance(cause),
                 "預期原因是 [" + expectedCause.getSimpleName() + "]，實際是 " +
                         cause.getClass().getSimpleName());
