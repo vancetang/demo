ab330fb03320981ddb4d01686a2f18a729904536
Vance <vance.tw@gmail.com>
2025-04-08 10:30:53 +0800

    feat(GlobalExceptionHandler): 更新全域例外狀況處理以支援 @RestController 註解

--- src/main/java/com/vance/demo/advice/GlobalExceptionHandler.java
+++ src/main/java/com/vance/demo/advice/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.vance.demo.advice;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.vance.demo.data.dto.ApiResult;
@@ -10,18 +11,25 @@ import com.vance.demo.enums.ResultCodeEnum;
 import com.vance.demo.exception.UserNotFoundException;
 
 /**
- * 全局異常處理
+ * 全域例外狀況處理 (僅處理標記有 @RestController 的類別)。
  * <p>
- * 異常處理類，用於處理全局異常，例如用戶不存在異常。
+ * 此例外狀況處理類別專門用於處理來自標記有 {@link RestController} 的控制器所引發的例外狀況，
+ * 例如 {@link UserNotFoundException}。
  * </p>
- * 
+ *
  * @author Vance
  */
-@RestControllerAdvice
+@RestControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiResult handleUserNotFound(UserNotFoundException ex) {
         return ApiResult.result(ResultCodeEnum.USER_NOT_FOUND);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResult handleIllegalArgument(IllegalArgumentException ex) {
+        return ApiResult.result(ResultCodeEnum.FAIL).message(ex.getMessage());
     }
 }
